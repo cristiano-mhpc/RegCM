6cf3e614a98cefe0b524caafd726f9de2b0d5c0a
Christian  Tica <ctica000@login05.leonardo.local>
2025-09-19 17:07:47 +0200

    added wrapper scripts profile_one_rank_new_nsys.sh and profile_STD_launch_per_rank.sh to profile single rank and all ranks, resprectively

---
+++ 1003_1003_50_1day_Benchmark/sequential_output/Non_MPS_stdpar/16gpu_16proc_profile/profile_STD_launch_per_rank.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Get local rank
+RANK=${SLURM_LOCALID}
+
+if [ -z "$RANK" ]; then
+  echo "SLURM_LOCALID not set; this script should be run under srun."
+  exit 1
+fi
+
+# GPU binding
+export CUDA_VISIBLE_DEVICES=$RANK
+export ACC_DEVICE_TYPE=nvidia
+export ACC_DEVICE_NUM=0
+
+# ----------------------------
+# Nsight Systems (newer CLI)
+# ----------------------------
+NSYS_BIN="/leonardo/prod/opt/compilers/nvhpc/25.3/binary/Linux_x86_64/25.3/compilers/bin/nsys"
+
+# Set binary and input paths
+export BINDIR=/leonardo/home/userexternal/ctica000/MS_thesis/RegCM/bin
+INPUT_FILE=EURR-3_namelist.in
+
+# Set a unique temp directory using TMPDIR only
+TMPDIR_BASE=$SCRATCH/tmp_nsys
+export TMPDIR=$TMPDIR_BASE/rank_${SLURM_PROCID}
+mkdir -p "$TMPDIR"
+
+# Set output path for Nsight profile
+NSYS_OUT="$SCRATCH/profile_new_nsys_16gpu/nsys_rank${SLURM_PROCID}"
+
+# Launch with Nsight profiling
+$NSYS_BIN profile \
+  --force-overwrite true \
+  --stats=true \
+  --sample=none \
+  --output "$NSYS_OUT" \
+  --trace=nvtx,mpi,cuda \
+  --mpi-impl=openmpi \
+  "$BINDIR/regcmMPICLM45_OPENACC_GPU_STDPAR" "$INPUT_FILE"
+
