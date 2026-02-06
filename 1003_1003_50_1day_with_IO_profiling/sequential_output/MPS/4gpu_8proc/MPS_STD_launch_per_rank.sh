446de9bdacade0bb9749cad6f08f9091d8986d56
Christian <ticachristian20@gmail.com>
2026-02-06 23:02:07 +0100

    Added MPS 1-day runs. The end-to-end runtime is twice.

--- 1003_1003_50_1day_with_IO_profiling/sequential_output/MPS/4gpu_8proc/MPS_STD_launch_per_rank.sh
+++ 1003_1003_50_1day_with_IO_profiling/sequential_output/MPS/4gpu_8proc/MPS_STD_launch_per_rank.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
+# launch_per_rank.sh
 
-RANK=${SLURM_LOCALID}
-if [ -z "$RANK" ]; then
-  echo "SLURM_LOCALID not set"
-  exit 1
-fi
+# Get the rank of the current MPI process
+LOCAL_RANK=$OMPI_COMM_WORLD_LOCAL_RANK
+GLOBAL_RANK=$OMPI_COMM_WORLD_RANK
 
-# Dynamically determine number of GPUs allocated on the node
-NUM_GPUS=${SLURM_GPUS_PER_NODE:-1} # Fallback to -1 if not set
+export CUDA_LAUNCH_BLOCKING=1     # pin runtime error to the exact line
+NUM_GPUS=${SLURM_GPUS_ON_NODE}
 
 # 4 GPUs shared among 8 ranks:
-GPU_ID=$(( RANK % NUM_GPUS ))
+GPU_ID=$(( LOCAL_RANK % 4 ))
 
-export CUDA_VISIBLE_DEVICES=$GPU_ID
+# Assign each local rank to a unique GPU on this node
+export CUDA_VISIBLE_DEVICES=$GPU_ID 
 export ACC_DEVICE_TYPE=nvidia
-export ACC_DEVICE_NUM=$GPU_ID
-export BINDIR=/leonardo/home/userexternal/ctica000/MS_thesis/RegCM/bin
+export BINDIR=/gpfs/home/ictp/ictp549985/RegCM/bin
 
-echo "[Wrapper][Rank $SLURM_PROCID][Local $RANK] using GPU $GPU_ID on $(hostname)"
+# Reduce contention for 2 ranks per GPU
+export CUDA_MPS_ACTIVE_THREAD_PERCENTAGE=50
 
-# Launch the binary 
-exec $BINDIR/regcmMPICLM45_OPENACC_GPU_STDPAR EURR-3_namelist.in 
+# Diagnostic 
+echo "[Wrapper][Rank $GLOBAL_RANK][Local $LOCAL_RANK] using GPU $GPU_ID on $(hostname)"
 
+# Launch the binary
+exec $BINDIR/regcmMPICLM45 EURR-3_namelist.in 
