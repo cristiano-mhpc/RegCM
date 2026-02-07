328b872a222ebbdd72a0555d9037fef2e508ce6d
Christian <ticachristian20@gmail.com>
2026-02-07 10:45:23 +0100

    Fused some do concurrent kernels in wafone in mod_moloch.F90

--- 1003_1003_50_1day_with_IO_profiling/sequential_output/Non_MPS_stdpar/Prod_16gpu_16proc_less_freq/STD_launch_per_rank.sh
+++ 1003_1003_50_1day_with_IO_profiling/sequential_output/Non_MPS_stdpar/Prod_16gpu_16proc_less_freq/STD_launch_per_rank.sh
@@ -17,7 +17,7 @@ export CUDA_LAUNCH_BLOCKING=1     # pin runtime error to the exact line
 # Assign each local rank to a unique GPU on this node
 export CUDA_VISIBLE_DEVICES=$LOCAL_RANK
 export ACC_DEVICE_TYPE=nvidia
-# export ACC_DEVICE_NUM=0
+export ACC_DEVICE_NUM=0
 
 export BINDIR=/gpfs/home/ictp/ictp549985/RegCM/bin
 
