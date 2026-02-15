1addda43db2cf6d05570e416b1e7b05a2d60c688
Christian  Tica <ctica000@login07.leonardo.local>
2026-02-15 09:50:20 +0100

    Added runs for 8gpus and 4gpus for 7days. Runtime errors appear very frequently.

---
+++ Benchmark/1003_1003_50_7days_with_IO_profiling/sequential_output/Non_MPS_stdpar/Production_4gpu_4proc_copy/STD_launch_per_rank.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# launch_per_rank.sh
+
+# Get the rank of the current MPI process
+RANK=${SLURM_LOCALID}
+
+# Optional safety check
+if [ -z "$RANK" ]; then
+  echo "SLURM_LOCALID not set; this script should be run under srun."
+  exit 1
+fi
+
+# Assign each local rank to a unique GPU on this node
+export CUDA_VISIBLE_DEVICES=$RANK
+export ACC_DEVICE_TYPE=nvidia
+export ACC_DEVICE_NUM=0
+export BINDIR=/leonardo/home/userexternal/ctica000/MS_thesis/ICTP_Regcm/RegCM/bin
+
+echo "[Wrapper][Rank $SLURM_PROCID][Local $SLURM_LOCALID] on $(hostname): CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+#
+# Launch the binary
+exec $BINDIR/regcmMPICLM45_OPENACC_STDPAR EURR-3_namelist.in 
