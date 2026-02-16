c02ab5f6d829c1886389a599f8055f57c0bfda82
Christian Tica <ticachristian20@gmail.com>
2026-02-16 23:01:11 +0100

    added runtimes for 3 days and 4 GPUs and 8 GPUs.

--- Benchmark/1003_1003_50_3days_with_IO_profiling/sequential_output/Non_MPS_stdpar/Production_16gpu_16proc_less_freq/STD_launch_per_rank.sh
+++ Benchmark/1003_1003_50_3days_with_IO_profiling/sequential_output/Non_MPS_stdpar/Production_16gpu_16proc_less_freq/STD_launch_per_rank.sh
@@ -2,21 +2,27 @@
 # launch_per_rank.sh
 
 # Get the rank of the current MPI process
-RANK=${SLURM_LOCALID}
+LOCAL_RANK=$OMPI_COMM_WORLD_LOCAL_RANK
+GLOBAL_RANK=$OMPI_COMM_WORLD_RANK
+
 
 # Optional safety check
-if [ -z "$RANK" ]; then
-  echo "SLURM_LOCALID not set; this script should be run under srun."
+if [ -z "$LOCAL_RANK" ]; then
+  echo "OMPI_COMM_WORLD_LOCAL_RANK not set; this script should be run under mpirun."
   exit 1
 fi
 
+export CUDA_LAUNCH_BLOCKING=1     # pin runtime error to the exact line
+
 # Assign each local rank to a unique GPU on this node
-export CUDA_VISIBLE_DEVICES=$RANK
+export CUDA_VISIBLE_DEVICES=$LOCAL_RANK
 export ACC_DEVICE_TYPE=nvidia
 export ACC_DEVICE_NUM=0
-export BINDIR=/leonardo/home/userexternal/ctica000/MS_thesis/RegCM/bin
 
-echo "[Wrapper][Rank $SLURM_PROCID][Local $SLURM_LOCALID] on $(hostname): CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
-#
+export BINDIR=/u/external/ctica/MS_thesis/RegCM/bin
+
+printf '[Wrapper][Rank %02d][Local %02d] on %s: CUDA_VISIBLE_DEVICES=%s ACC_DEVICE_NUM=%s\n' \
+"$GLOBAL_RANK" "$LOCAL_RANK" "$(hostname)" "$CUDA_VISIBLE_DEVICES" "$ACC_DEVICE_NUM"
+
 # Launch the binary
-exec $BINDIR/regcmMPICLM45_OPENACC_GPU_STDPAR EURR-3_namelist.in 
+exec $BINDIR/regcmMPICLM45 EURR-3_namelist.in 
