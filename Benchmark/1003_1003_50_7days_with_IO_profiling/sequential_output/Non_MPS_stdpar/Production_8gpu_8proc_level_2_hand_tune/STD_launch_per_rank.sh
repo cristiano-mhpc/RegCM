0bfdd691c60db01bd4897a198c614b2584dd0900
Christian Tica <ticachristian20@gmail.com>
2026-04-28 17:53:52 +0200

    Loaded the software environment so I can get the information about ompi_cuda.txt and ompi_ucx.txt. Since the came out empty previously.

--- Benchmark/1003_1003_50_7days_with_IO_profiling/sequential_output/Non_MPS_stdpar/Production_8gpu_8proc_level_2_hand_tune/STD_launch_per_rank.sh
+++ Benchmark/1003_1003_50_7days_with_IO_profiling/sequential_output/Non_MPS_stdpar/Production_8gpu_8proc_level_2_hand_tune/STD_launch_per_rank.sh
@@ -1,107 +1,82 @@
 #!/bin/bash
-# ORFEO optimized per-rank launcher
+# ORFEO Level 2 wrapper: topology-derived, no-guessing version
 
-LOCAL_RANK=$OMPI_COMM_WORLD_LOCAL_RANK
-GLOBAL_RANK=$OMPI_COMM_WORLD_RANK
+LOCAL_RANK=${OMPI_COMM_WORLD_LOCAL_RANK}
+GLOBAL_RANK=${OMPI_COMM_WORLD_RANK}
 
 if [ -z "$LOCAL_RANK" ]; then
-  echo "ERROR: Must be run with mpirun"
+  echo "ERROR: OMPI_COMM_WORLD_LOCAL_RANK is not set. Run under mpirun."
   exit 1
 fi
 
-########################################
-# GPU binding
-########################################
-export CUDA_VISIBLE_DEVICES=$LOCAL_RANK
+# 1 rank -> 1 GPU
+export CUDA_VISIBLE_DEVICES=${LOCAL_RANK}
 export ACC_DEVICE_TYPE=nvidia
 export ACC_DEVICE_NUM=0
 
-########################################
-# REMOVE DEBUG SLOWDOWN
-########################################
+# Remove debug slowdown if it was inherited
 unset CUDA_LAUNCH_BLOCKING
 
-########################################
-# UCX tuning (safe baseline)
-########################################
-export UCX_TLS=cma,rc,mm,cuda_copy,cuda_ipc
-export UCX_RNDV_THRESH=128
-export UCX_RNDV_FRAG_MEM_TYPE=cuda
-export UCX_RNDV_FRAG_SIZE=cuda:32M
-
-########################################
-# CPU + NUMA binding (CRITICAL)
-########################################
-
-case ${LOCAL_RANK} in
-0)
-  CPU_RANGE=0-13
-  MEM_NODE=0
-  NIC=mlx5_0:1
-  ;;
-1)
-  CPU_RANGE=14-27
-  MEM_NODE=1
-  NIC=mlx5_1:1
-  ;;
-2)
-  CPU_RANGE=28-41
-  MEM_NODE=2
-  NIC=mlx5_2:1
-  ;;
-3)
-  CPU_RANGE=42-55
-  MEM_NODE=3
-  NIC=mlx5_3:1
-  ;;
-4)
-  CPU_RANGE=56-69
-  MEM_NODE=4
-  NIC=mlx5_4:1
-  ;;
-5)
-  CPU_RANGE=70-83
-  MEM_NODE=5
-  NIC=mlx5_5:1
-  ;;
-6)
-  CPU_RANGE=84-97
-  MEM_NODE=6
-  NIC=mlx5_6:1
-  ;;
-7)
-  CPU_RANGE=98-111
-  MEM_NODE=7
-  NIC=mlx5_7:1
-  ;;
-esac
-
-########################################
-# NIC binding (UCX)
-########################################
-export UCX_NET_DEVICES=$NIC
-
-########################################
-# Threading (match Everett)
-########################################
+# Match the production CPU allocation model
 export OMP_NUM_THREADS=14
 export MKL_NUM_THREADS=14
 export OMP_PROC_BIND=true
 export OMP_PLACES=cores
 
-########################################
-# Debug print
-########################################
-echo "[Rank $GLOBAL_RANK | Local $LOCAL_RANK]"
-echo "  GPU: $CUDA_VISIBLE_DEVICES"
-echo "  CPU: $CPU_RANGE"
-echo "  NUMA: $MEM_NODE"
-echo "  NIC: $NIC"
+# Exact GPU -> CPU/NUMA/NIC mapping from dgx004 topology
+case "${LOCAL_RANK}" in
+  0)
+    CPU_RANGE="0-13"
+    MEM_NODE="0"
+    NIC_DEV="mlx5_0:1"
+    ;;
+  1)
+    CPU_RANGE="28-41"
+    MEM_NODE="2"
+    NIC_DEV="mlx5_1:1"
+    ;;
+  2)
+    CPU_RANGE="42-55"
+    MEM_NODE="3"
+    NIC_DEV="mlx5_2:1"
+    ;;
+  3)
+    CPU_RANGE="14-27"
+    MEM_NODE="1"
+    NIC_DEV="mlx5_3:1"
+    ;;
+  4)
+    CPU_RANGE="56-69"
+    MEM_NODE="4"
+    NIC_DEV="mlx5_4:1"
+    ;;
+  5)
+    CPU_RANGE="84-97"
+    MEM_NODE="6"
+    NIC_DEV="mlx5_5:1"
+    ;;
+  6)
+    CPU_RANGE="98-111"
+    MEM_NODE="7"
+    NIC_DEV="mlx5_6:1"
+    ;;
+  7)
+    CPU_RANGE="70-83"
+    MEM_NODE="5"
+    NIC_DEV="mlx5_7:1"
+    ;;
+  *)
+    echo "ERROR: unexpected LOCAL_RANK=${LOCAL_RANK}"
+    exit 1
+    ;;
+esac
 
-########################################
-# Launch
-########################################
+# Use the topology-matched HCA only
+export UCX_NET_DEVICES="${NIC_DEV}"
+
 BINDIR=/orfeo/cephfs/home/external/ctica/ICTP_RegCM/RegCM/bin
 
-exec numactl --physcpubind=$CPU_RANGE --membind=$MEM_NODE \
-  $BINDIR/regcmMPICLM45 EURR-3_namelist.in
+echo "[Wrapper][Rank ${GLOBAL_RANK}][Local ${LOCAL_RANK}] host=$(hostname) GPU=${CUDA_VISIBLE_DEVICES} CPU=${CPU_RANGE} NUMA=${MEM_NODE} NIC=${NIC_DEV}"
+
+exec numactl --physcpubind="${CPU_RANGE}" --membind="${MEM_NODE}" \
+  "${BINDIR}/regcmMPICLM45" EURR-3_namelist.in
